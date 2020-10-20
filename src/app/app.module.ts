e8dcc54f7fa0298c674d37cd31717b1d72e3d7e6
mcstagg <32423173+mcstagg@users.noreply.github.com>
2020-10-20 14:54:24 -0500

    app created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuitarGodsComponent } from './guitar-gods/guitar-gods.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GuitarGodsComponent
   ],
   imports: [
     BrowserModule,
